a6ab187e962f9dfd749072959e872b98bee14af9
Nichole Boothman <nbooth731@gmail.com>
2022-04-04 16:59:49 -0400

    props drilling

---
+++ src/components/counter.jsx
@@ -0,0 +1,46 @@
+import { Fragment, Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: this.props.value
+    }
+    formatCount() {
+        const { count } = this.state
+        return count === 0 ? 'Zero' : count
+    }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-"
+        classes += (this.state.count === 0) ? 'warning' : 'primary'
+        return classes
+    }
+
+    // constructor() {
+    //     super()
+    //     this.handleIncrementBtn = this.handleIncrementBtn.bind(this)
+    // }
+    // renderTags() {
+    //     if (this.state.tags.length === 0) {
+    //         return <p>There are no tags</p>
+    //     } else {
+    //        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    //     }
+    // }
+
+    handleIncrementBtn = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
+    render() { 
+        return (
+            <Fragment>
+                <h4>{this.props.id}</h4>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrementBtn} 
+                className='btn btn-secondary btn-sm'>Increment</button>
+            </Fragment>
+            
+        );
+    }
+}
+ 
+export default Counter;
