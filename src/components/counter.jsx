0fa7948bde2355ed921490544b31675f78c1c617
Nichole Boothman <nbooth731@gmail.com>
2022-04-05 11:26:00 -0400

    complete

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -37,6 +37,7 @@ class Counter extends Component {
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrementBtn} 
                 className='btn btn-secondary btn-sm'>Increment</button>
+                <button onClick={() => this.props.onDelete(this.props.id)} className='btn btn-danger btn-sm m-2'>Delete</button>
             </Fragment>
             
         );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,11 +11,21 @@ class Counters extends Component {
             {id: 5, value: 0}
         ]
      } 
+
+     handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(c => c.id !== counterId)
+        this.setState({counters})
+     }
+
     render() { 
         return (
         <div>
             {this.state.counters.map(counter => 
-            <Counter key={counter.id} value={counter.value} id = {counter.id}/>)}
+            <Counter 
+                key={counter.id} 
+                onDelete={this.handleDelete} 
+                value={counter.value} 
+                id = {counter.id}/>)}
         </div>);
     }
 }
